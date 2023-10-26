40314d7ef4a9a7cd341e63d8401b87a08855f44d
6yza6yza <52315168+6yza6yza@users.noreply.github.com>
2023-10-26 14:32:57 +0200

    project initialization

--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -1,10 +1,14 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
 
 @Entity({name: 'categories'})
 export class Category {
     @PrimaryGeneratedColumn()
     id: number;
-  
+
     @Column()
     name: string;
+
+    
+    @Column()
+    description: string;
 }
